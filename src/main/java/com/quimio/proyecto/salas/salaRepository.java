adc1e1bc7766d53d6c6db9da07427b48253fb8ef
TDocmac <jtdocmac@uc.cl>
2020-07-19 20:59:08 -0400

    Delete salaRepository.java

--- src/main/java/com/quimio/proyecto/salas/salaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quimio.proyecto.salas;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface salaRepository extends CrudRepository<sala, Long> {
-    
-}
