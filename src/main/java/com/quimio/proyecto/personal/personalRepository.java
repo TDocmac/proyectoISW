01d883d7a52a13113b1f244e15d931b1d86d0c43
TDocmac <jtdocmac@uc.cl>
2020-07-19 20:57:44 -0400

    Delete personalRepository.java

--- src/main/java/com/quimio/proyecto/personal/personalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quimio.proyecto.personal;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface personalRepository extends CrudRepository<personal, Long> {
-    
-}
