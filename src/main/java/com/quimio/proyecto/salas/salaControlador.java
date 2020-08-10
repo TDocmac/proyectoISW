159c3d8b3322598addc5baf83f0a6aa81176c120
TDocmac <jtdocmac@uc.cl>
2020-08-10 01:33:45 -0400

    Update salaControlador.java

--- src/main/java/com/quimio/proyecto/salas/salaControlador.java
+++ src/main/java/com/quimio/proyecto/salas/salaControlador.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
 @RequestMapping("/api/salas")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE})
 public class salaControlador {
 
     @Autowired
